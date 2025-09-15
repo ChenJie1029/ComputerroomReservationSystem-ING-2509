41eee005fccdec9ef816b5413230ca863855783d
ChenJie <1439475057@qq.com>
2025-09-15 08:13:45 +0800

    登录函数接口封装

--- 机房预约系统/机房预约系统.cpp
+++ 机房预约系统/机房预约系统.cpp
@@ -1,6 +1,60 @@
 #include<iostream>
+#include<string>
+#include "Identity.h"
+#include "globalFile.h"
 
 using namespace std;
+
+//登录功能  参数1：操作文件名、参数2：操作身份的类型
+void LoginIn(string fileName, int type) {
+	//父类指针，用于指向子类对象
+	Identity* person = NULL;
+
+	//读文件
+	ifstream ifs;
+	ifs.open(fileName, ios::in);
+
+	//判断文件是否存在
+	if (!ifs.is_open()) {
+		cout << "文件不存在" << endl;
+		ifs.close();
+		return;
+	}
+
+	//准备接受用户的信息
+	int id = 0;
+	string name;
+	string pwd;
+
+	//判断身份
+	if (type == 1) {//学生身份
+		cout << "请输入你的学号：" << endl;
+		cin >> id;
+	}
+	else if (type == 2) {//老师身份
+		cout << "请输入你的职工号：" << endl;
+		cin >> id;
+	}
+
+	cout << "请输入用户名：" << endl;
+	cin >> name;
+	cout << "请输入密码：" << endl;
+	cin >> pwd;
+
+	if (type == 1) {//学生身份验证
+
+	}
+	else if (type == 2) {//教师身份验证
+
+	}
+	else if (type == 3) {//管理员身份验证
+
+	}
+
+	cout << "登录失败" << endl;
+	system("pause");
+	system("cls");
+}
 
 int main() {
 	int select = 0;
@@ -17,11 +71,14 @@ int main() {
 
 		switch (select)
 		{
-		case 1:
+		case 1://学生身份
+			LoginIn(STRUNCATE, 1);
 			break;
 		case 2:
+			LoginIn(TEACHER_FILE, 2);
 			break;
 		case 3:
+			LoginIn(ADMIN_FILE, 3);
 			break;
 		case 0:
 			cout << "欢迎下次使用！" << endl;
