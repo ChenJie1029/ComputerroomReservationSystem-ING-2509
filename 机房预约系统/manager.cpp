ea81e296ab839641e8f82c10639ef2ab0505ec71
ChenJie <1439475057@qq.com>
2025-09-16 08:32:46 +0800

    管理员子菜单搭建以及注销实现

--- 机房预约系统/manager.cpp
+++ 机房预约系统/manager.cpp
@@ -5,12 +5,19 @@ Manager::Manager() {
 
 }
 //有参构造
-Manager::Manager(string name, string pwd) {
-
+Manager::Manager(string name, string pwd) {//初始化管理员信息
+	this->m_Name = name;
+	this->m_Pwd = pwd;
 }
 //菜单界面  虚构重写
 void Manager::operMenu() {
-
+	cout << "欢迎管理员:" << this->m_Name << "登录" << endl;
+	cout << "1.添加账号" << endl;
+	cout << "2.查看账号" << endl;
+	cout << "3.查看机房" << endl;
+	cout << "4.清空预约" << endl;
+	cout << "0.注销登录" << endl;
+	cout << "请选择您的操作：" << endl;
 }
 
 //添加账号
