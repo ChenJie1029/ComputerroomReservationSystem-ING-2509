83fe3cf9e780b73112806379da4ba5bf77811788
ChenJie <1439475057@qq.com>
2025-09-21 15:22:25 +0800

    教师显示所有预约功能实现

--- 机房预约系统/teacher.cpp
+++ 机房预约系统/teacher.cpp
@@ -22,7 +22,39 @@ void Teacher::operMenu() {
 
 //查看所有学生的预约
 void Teacher::showAllOrder() {
+	OrderFile of;
+	if (of.m_Size == 0) {
+		cout << "无预约记录" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
 
+	for (int i = 0; i < of.m_Size; i++) {
+		cout << i + 1 << "、";
+		cout << "预约日期: 周" << of.m_orderData[i]["date"];
+		cout << "时间段：" << (of.m_orderData[i]["interval"] == "1" ？ "上午" : "下午");
+		cout << "学号：" << of.m_orderData[i]["stuId"];
+		cout << "姓名：" << of.m_orderData[i]["stuName"];
+		cout << "机房编号：" << of.m_orderData[i]["roomId"];
+		string status = "状态：";
+		//1.审核中  2.已预约  -1.预约失败  0.取消预约
+		if (of.m_orderData[i]["status"] == "1") {
+			status += "审核中";
+		}
+		else if (of.m_orderData[i]["status"] == "2") {
+			status += "预约成功";
+		}
+		else if (of.m_orderData[i]["status"] == "-1") {
+			status += "预约失败，审核未通过";
+		}
+		else {
+			status += "预约已经取消";
+		}
+		cout << status << endl;
+	}
+	system("pause");
+	system("cls");
 }
 //审核预约
 void Teacher::valiOrder() {
--- 机房预约系统/teacher.h
+++ 机房预约系统/teacher.h
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string>
 #include "Identity.h"
+#include "orderFile.h"
 
 using namespace std;
 
